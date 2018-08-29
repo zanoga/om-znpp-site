eddb302be58269d18f362e2968b5313ca689cf18
Alex <zanoga1@gmail.com>
2018-08-29 13:54:16 +0300

    Create category.index page

--- resources/views/admin/layouts/app_admin.blade.php
+++ resources/views/admin/layouts/app_admin.blade.php
@@ -16,6 +16,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -38,7 +39,7 @@
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown"
                         role="button" aria-expanded="false">Блог</a>
                         <ul class="dropdown-menu" role="menu">
-                          <li><a href="#">Категории</a></li>
+                          <li><a href="{{route('admin.category.index')}}">Категории</a></li>
                           <li><a href="#">Фото</a></li>
                           <li><a href="#">Комиcсии</a></li>
                         </ul>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@
 
 Route::group(['prefix'=>'admin', 'namespace'=>'Admin', 'middleware'=>['auth']], function() {
   Route::get('/', 'DashboadrController@dashboard')->name('admin.index');
+  Route::resource('/category', 'CategoryController', ['as'=>'admin']);
 });
 
 Route::get('/', function () {
