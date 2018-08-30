6b5973c97d4baa6dae88d0a26fb5f41ef5b2630f
Alex <zanoga1@gmail.com>
2018-08-30 16:41:18 +0300

    adds Adds.Edit.Update articles

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -104,6 +104,8 @@ class ArticleController extends Controller
      */
     public function destroy(Article $article)
     {
-        //
+        $article->categories()->detach();
+        $article->delete();
+        return redirect()->route('admin.article.index');
     }
 }
